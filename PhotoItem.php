2b3b123a3d1075f5c492c698a2ed858aa2f6f6ac
Andrew Houle <andrewhoule@gmail.com>
2013-04-25 09:51:54 -0400

    Formfield updates, consistency updates, validation for images

--- PhotoItem.php
+++ PhotoItem.php
@@ -3,7 +3,7 @@
 class PhotoItem extends DataObject { 
 	
 	static $db = array (
-	   'SortID' => 'Int',
+	   	"SortID" => "Int",
 		"Caption" => "Text"
 	);
 	
@@ -16,16 +16,18 @@ class PhotoItem extends DataObject {
 	public static $default_sort = 'SortID Asc';
 	
 	public function getCMSFields() {
+		$imgfield = UploadField::create('Photo');
+      	$imgfield->getValidator()->allowedExtensions = array('jpg','jpeg','gif','png');
 		return new FieldList(
-		   new UploadField('Photo'),
-			new TextField('Caption')
+		   	$imgfield,
+			TextField::create('Caption')
 		);
 	}
 	
 	static $summary_fields = array (
-      'CaptionExcerpt' => 'Caption',
-      'Thumbnail' => 'Photo'
-   );
+      	'CaptionExcerpt' => 'Caption',
+      	'Thumbnail' => 'Photo'
+   	);
 	
 	public function Thumbnail() {
 		$Image = $this->Photo();
@@ -36,30 +38,30 @@ class PhotoItem extends DataObject {
 	}
 	
 	function CaptionExcerpt($length = 75) {
-   	$text = strip_tags($this->Caption);
-   	$length = abs((int)$length);
-   	if(strlen($text) > $length) {
-   		$text = preg_replace("/^(.{1,$length})(\s.*|$)/s", '\\1 ...', $text);
-   	}
-   	return $text;
-   }
+		$text = strip_tags($this->Caption);
+		$length = abs((int)$length);
+		if(strlen($text) > $length) {
+			$text = preg_replace("/^(.{1,$length})(\s.*|$)/s", '\\1 ...', $text);
+		}
+		return $text;
+	}
 	
 	public function getAlbums() {
-      $albums = PhotoAlbum::get()->sort('Created DESC');
-      if($albums->Exists()) {
-         return $albums->map('ID', 'Name', 'Please Select');
-      }
-      else { 
-        return array('No albums found');
-      }
-   }
+		$albums = PhotoAlbum::get()->sort('Created DESC');
+		if($albums->Exists()) {
+		 	return $albums->map('ID', 'Name', 'Please Select');
+		}
+		else { 
+			return array('No albums found');
+		}
+	}
 	
 	public function PhotoCropped($x=120,$y=120) {
-		 return $this->Photo()->CroppedImage($x,$y);
+		return $this->Photo()->CroppedImage($x,$y);
 	}
 	
 	public function PhotoSized($x=700,$y=700) {
-		 return $this->Photo()->SetRatioSize($x,$y);
+		return $this->Photo()->SetRatioSize($x,$y);
 	}
 	
 }
