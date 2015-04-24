293c9df504b5e82eec0c980067a2516d0d368762
Andrew Houle <andrewhoule@gmail.com>
2015-04-24 12:46:32 -0400

    Adding config options for where to default new albums and photos

--- code/PhotoAlbum.php
+++ code/PhotoAlbum.php
@@ -2,133 +2,136 @@
 
 class PhotoAlbum extends DataObject {
 
-	private static $db = array (
-	   'SortID' => 'Int',
-		'Name' => 'Text',
-		'Description' => 'HTMLText'
-	);
+  private static $db = array (
+    'SortID' => 'Int',
+    'Name' => 'Text',
+    'Description' => 'HTMLText'
+  );
 
-	private static $has_one = array (
-		'PhotoGallery' => 'PhotoGallery',
-		'AlbumCover' => 'Image'
-	);
+  private static $has_one = array (
+    'PhotoGallery' => 'PhotoGallery',
+    'AlbumCover' => 'Image'
+  );
 
-	private static $has_many = array (
-		'PhotoItems' => 'PhotoItem'
-	);
+  private static $has_many = array (
+    'PhotoItems' => 'PhotoItem'
+  );
 
-	private static $summary_fields = array (
-		'Thumbnail' => 'Cover Photo',
-		'Name' => 'Name',
-		'DescriptionExcerpt' => 'Description'
-	);
+  private static $summary_fields = array (
+    'Thumbnail' => 'Cover Photo',
+    'Name' => 'Name',
+    'DescriptionExcerpt' => 'Description'
+  );
 
-	public function canCreate($Member = null) { return true; }
-	public function canEdit($Member = null) { return true; }
-	public function canView($Member = null) { return true; }
-	public function canDelete($Member = null) { return true; }
+  public function canCreate($Member = null) { return true; }
+  public function canEdit($Member = null) { return true; }
+  public function canView($Member = null) { return true; }
+  public function canDelete($Member = null) { return true; }
 
    private static $default_sort = 'SortID ASC';
 
    public function PageFolder() {
-   	if($name = $this->getComponent('PhotoGallery')->MenuTitle) {
-   		$string = strtolower($name);
-			$string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
-			$string = preg_replace("/[\s-]+/", " ", $string);
-			$string = preg_replace("/[\s_]/", "-", $string);
-			return $string;
-   	}
-   	else {
-   		return "photogallery";
-   	}
+    if($name = $this->getComponent('PhotoGallery')->MenuTitle) {
+      $string = strtolower($name);
+      $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+      $string = preg_replace("/[\s-]+/", " ", $string);
+      $string = preg_replace("/[\s_]/", "-", $string);
+      return $string;
+    }
+    else {
+      return "photogallery";
+    }
    }
 
    public function AlbumFolder() {
-   	if($name = $this->Name) {
-   		$string = strtolower($name);
-			$string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
-			$string = preg_replace("/[\s-]+/", " ", $string);
-			$string = preg_replace("/[\s_]/", "-", $string);
-			return $string;
-   	}
-   	else {
-   		return "album";
-   	}
+    if($name = $this->Name) {
+      $string = strtolower($name);
+      $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+      $string = preg_replace("/[\s-]+/", " ", $string);
+      $string = preg_replace("/[\s_]/", "-", $string);
+      return $string;
+    }
+    else {
+      return "album";
+    }
    }
 
-	public function getCMSFields() {
-		if($this->ID == 0) {
-			$PhotosGridField = TextField::create('PhotosDisclaimer')->setTitle('Photos')->setDisabled(true)->setValue('You can add photos once you have saved the record for the first time.');
-			$ImageField = TextField::create('AlbumCoverDisclaimer')->setTitle('Album Cover Photo')->setDisabled(true)->setValue('You can add an album cover once you have saved the record for the first time.');
-		}
-		else {
-			$BulkUploadComponent = new GridFieldBulkUpload();
-			$BulkUploadComponent->setUfSetup('setFolderName',"photogallery/" . $this->PageFolder() . "/" . $this->AlbumFolder());
-			$PhotosGridField = new GridField(
-	         "PhotoItems",
-	         "Photos",
-	         $this->PhotoItems(),
-	         GridFieldConfig::create()
-					->addComponent(new GridFieldToolbarHeader())
-					->addComponent(new GridFieldAddNewButton("toolbar-header-right"))
-					->addComponent(new GridFieldSortableHeader())
-					->addComponent(new GridFieldDataColumns())
-					->addComponent(new GridFieldPaginator(50))
-					->addComponent(new GridFieldEditButton())
-					->addComponent(new GridFieldDeleteAction())
-					->addComponent(new GridFieldDetailForm())
-					->addComponent(new GridFieldFilterHeader())
-					->addComponent($BulkUploadComponent)
-					->addComponent(new GridFieldSortableRows("SortID"))
-	      );
-	      $ImageField = UploadField::create("AlbumCover")->setTitle("Album Cover Photo");
-	    	$ImageField->folderName = "photogallery/" . $this->PageFolder();
-	      $ImageField->getValidator()->allowedExtensions = array("jpg","jpeg","gif","png");
-		}
-	   $Fields = new FieldList(
-         TextField::create("Name"),
-         TextareaField::create("Description"),
-         $ImageField,
-         $PhotosGridField
-	   );
-	   $this->extend('updateCMSFields',$Fields);
-      return $Fields;
-	}
+  public function getCMSFields() {
+    if($this->ID == 0) {
+      $PhotosGridField = TextField::create('PhotosDisclaimer')->setTitle('Photos')->setDisabled(true)->setValue('You can add photos once you have saved the record for the first time.');
+      $ImageField = TextField::create('AlbumCoverDisclaimer')->setTitle('Album Cover Photo')->setDisabled(true)->setValue('You can add an album cover once you have saved the record for the first time.');
+    }
+    else {
+      $BulkUploadComponent = new GridFieldBulkUpload();
+      $BulkUploadComponent->setUfSetup('setFolderName',"photogallery/" . $this->PageFolder() . "/" . $this->AlbumFolder());
+      $PhotosGridField = new GridField(
+           "PhotoItems",
+           "Photos",
+           $this->PhotoItems(),
+           GridFieldConfig::create()
+          ->addComponent(new GridFieldToolbarHeader())
+          ->addComponent(new GridFieldAddNewButton("toolbar-header-right"))
+          ->addComponent(new GridFieldSortableHeader())
+          ->addComponent(new GridFieldDataColumns())
+          ->addComponent(new GridFieldPaginator(50))
+          ->addComponent(new GridFieldEditButton())
+          ->addComponent(new GridFieldDeleteAction())
+          ->addComponent(new GridFieldDetailForm())
+          ->addComponent(new GridFieldFilterHeader())
+          ->addComponent($BulkUploadComponent)
+          ->addComponent($sortable = new GridFieldSortableRows('SortID'))
+      );
+      if($this->getComponent('PhotoGallery')->PhotoDefaultTop == true) {
+        $sortable->setAppendToTop(true);
+      }
+      $ImageField = UploadField::create("AlbumCover")->setTitle("Album Cover Photo");
+      $ImageField->folderName = "photogallery/" . $this->PageFolder();
+      $ImageField->getValidator()->allowedExtensions = array("jpg","jpeg","gif","png");
+    }
+    $Fields = new FieldList(
+      TextField::create("Name"),
+      TextareaField::create("Description"),
+      $ImageField,
+      $PhotosGridField
+    );
+    $this->extend('updateCMSFields',$Fields);
+    return $Fields;
+  }
 
-	public function Thumbnail() {
-		$Image = $this->AlbumCover();
-		if ($Image)
-			return $Image->CMSThumbnail();
-		else
-			return null;
-	}
+  public function Thumbnail() {
+    $Image = $this->AlbumCover();
+    if ($Image)
+      return $Image->CMSThumbnail();
+    else
+      return null;
+  }
 
-	public function DescriptionExcerpt($length=75) {
-	  	$text = strip_tags($this->Description);
-	  	$length = abs((int)$length);
-	  	if(strlen($text) > $length) {
-	  		$text = preg_replace("/^(.{1,$length})(\s.*|$)/s", '\\1...', $text);
-	  	}
-		return $text;
+  public function DescriptionExcerpt($length=75) {
+      $text = strip_tags($this->Description);
+      $length = abs((int)$length);
+      if(strlen($text) > $length) {
+        $text = preg_replace("/^(.{1,$length})(\s.*|$)/s", '\\1...', $text);
+      }
+    return $text;
    }
 
-	public function PhotoCropped($x=120,$y=120) {
-		$width = $this->PhotoGallery()->AlbumThumbnailWidth;
-		$height = $this->PhotoGallery()->AlbumThumbnailWidth;
-		if($width != 0)
-			$x = $width;
-		if($height != 0)
-			$y = $height;
-		if($this->AlbumCover()->exists())
-		 	return $this->AlbumCover()->CroppedImage($x,$y);
-		else {
-			if($this->PhotoGallery()->DefaultAlbumCover()->exists()) {
-				return $this->PhotoGallery()->DefaultAlbumCover()->CroppedImage($x,$y);
-			}
-		}
-	}
+  public function PhotoCropped($x=120,$y=120) {
+    $width = $this->PhotoGallery()->AlbumThumbnailWidth;
+    $height = $this->PhotoGallery()->AlbumThumbnailWidth;
+    if($width != 0)
+      $x = $width;
+    if($height != 0)
+      $y = $height;
+    if($this->AlbumCover()->exists())
+      return $this->AlbumCover()->CroppedImage($x,$y);
+    else {
+      if($this->PhotoGallery()->DefaultAlbumCover()->exists()) {
+        return $this->PhotoGallery()->DefaultAlbumCover()->CroppedImage($x,$y);
+      }
+    }
+  }
 
-	public function Link() {
+  public function Link() {
       if($PhotoGallery = $this->PhotoGallery()) {
          $Action = "album/" . $this->ID;
          return $PhotoGallery->Link($Action);
@@ -136,41 +139,33 @@ class PhotoAlbum extends DataObject {
    }
 
    public function Photos() {
-		$photoset = new ArrayList();
+    $photoset = new ArrayList();
       $this->extend('GetItems', $photoset);
       if (!$photoset->count()) {
          $photos = PhotoItem::get()->filter("PhotoAlbumID", $this->ID);
-   	   if ($photos) {
-      	   foreach ($photos AS $photo) {
+       if ($photos) {
+           foreach ($photos AS $photo) {
                if ($photo->getComponent("Photo")->exists()) {
                   $photoset->push($photo);
                }
             }
-        	}
-     	}
+          }
+      }
       return $photoset;
    }
 
    public function PhotoCount() {
-   	return $this->Photos()->count();
+    return $this->Photos()->count();
    }
 
    public function getTitle() {
-    	return $this->Name;
+      return $this->Name;
    }
 
    public function PaginatedPhotos() {
       $paginatedphotos = new PaginatedList($this->Photos(), $this->request);
       $paginatedphotos->setPageLength($this->PhotosPerPage);
       return $paginatedphotos;
-   }
-   
-   public function onBeforeDelete() {
-      $photos = PhotoItem::get()->filter("PhotoAlbumID", $this->ID);
-      if ($photos) {
-            $photos->removeAll();
-      }
-      return parent::onBeforeDelete();
    }
 
 }
--- code/PhotoGallery.php
+++ code/PhotoGallery.php
@@ -2,174 +2,183 @@
 
 class PhotoGallery extends Page {
  
-	private static $db = array(
-		'AlbumsPerPage' => 'Int',
-		'AlbumThumbnailHeight' => 'Int',
-		'AlbumThumbnailWidth' => 'Int',
-		'ShowAllPhotoAlbums' => 'Boolean',
-		'PhotosPerPage' => 'Int',
-		'PhotoThumbnailHeight' => 'Int',
-		'PhotoThumbnailWidth' => 'Int',
-		'PhotoFullHeight' => 'Int',
-		'PhotoFullWidth' => 'Int'
-	);
+  private static $db = array(
+    'AlbumsPerPage' => 'Int',
+    'AlbumThumbnailHeight' => 'Int',
+    'AlbumThumbnailWidth' => 'Int',
+    'AlbumDefaultTop' => 'Boolean',
+    'ShowAllPhotoAlbums' => 'Boolean',
+    'PhotosPerPage' => 'Int',
+    'PhotoThumbnailHeight' => 'Int',
+    'PhotoThumbnailWidth' => 'Int',
+    'PhotoFullHeight' => 'Int',
+    'PhotoFullWidth' => 'Int',
+    'PhotoDefaultTop' => 'Boolean'
+  );
    
-	private static $has_one = array (
-		'DefaultAlbumCover' => 'Image'
-	);
+  private static $has_one = array (
+    'DefaultAlbumCover' => 'Image'
+  );
 
-	private static $has_many = array (
-		'PhotoAlbums' => 'PhotoAlbum',
-		'PhotoItems' => 'PhotoItem'
-	);
+  private static $has_many = array (
+    'PhotoAlbums' => 'PhotoAlbum',
+    'PhotoItems' => 'PhotoItem'
+  );
    
-	private static $defaults = array(
-		'AlbumsPerPage' => '6',
-		'PhotosPerPage' => '20',
-		'ShowAllPhotoAlbums' => true,
-		'AlbumThumbnailWidth' => '200',
-		'AlbumThumbnailHeight' => '200',
-		'PhotoThumbnailWidth' => '150',
-		'PhotoThumbnailHeight' => '150',
-		'PhotoFullWidth' => '700',
-		'PhotoFullHeight' => '700'
-	);
+  private static $defaults = array(
+    'AlbumsPerPage' => '6',
+    'PhotosPerPage' => '20',
+    'ShowAllPhotoAlbums' => true,
+    'AlbumThumbnailWidth' => '200',
+    'AlbumThumbnailHeight' => '200',
+    'AlbumDefaultTop' => true,
+    'PhotoThumbnailWidth' => '150',
+    'PhotoThumbnailHeight' => '150',
+    'PhotoFullWidth' => '700',
+    'PhotoFullHeight' => '700',
+    'PhotoDefaultTop' => true
+  );
    
-	private static $icon = "photogallery/images/photogallery";
+  private static $icon = "photogallery/images/photogallery";
    
-	public function getCMSFields() {
-		$DefaultAlbumCoverField = UploadField::create('DefaultAlbumCover');
-		$DefaultAlbumCoverField->folderName = "PhotoGallery"; 
-		$DefaultAlbumCoverField->getValidator()->allowedExtensions = array('jpg','jpeg','gif','png');
-		$fields = parent::getCMSFields();
-		$AlbumsGridField = new GridField(
-			"PhotoAlbums",
-			"Album",
-			$this->PhotoAlbums(),
-			GridFieldConfig::create()
-				->addComponent(new GridFieldToolbarHeader())
-				->addComponent(new GridFieldAddNewButton('toolbar-header-right'))
-				->addComponent(new GridFieldSortableHeader())
-				->addComponent(new GridFieldDataColumns())
-				->addComponent(new GridFieldPaginator(50))
-				->addComponent(new GridFieldEditButton())
-				->addComponent(new GridFieldDeleteAction())
-				->addComponent(new GridFieldDetailForm())
-				->addComponent(new GridFieldFilterHeader())
-				->addComponent(new GridFieldSortableRows('SortID'))
-		);
-		$fields->addFieldToTab("Root.Albums", $AlbumsGridField);
-		$fields->addFieldToTab("Root.Config", HeaderField::create("Album Settings"));
-		$fields->addFieldToTab("Root.Config", $DefaultAlbumCoverField);
-		$fields->addFieldToTab("Root.Config", SliderField::create('AlbumsPerPage','Number of Albums Per Page',1,25));
-		$fields->addFieldToTab("Root.Config", SliderField::create("AlbumThumbnailWidth","Album Cover Thumbnail Width",50,400));
-		$fields->addFieldToTab("Root.Config", SliderField::create("AlbumThumbnailHeight","Album Cover Thumbnail Height",50,400));
-		$fields->addFieldToTab("Root.Config", CheckboxField::create("ShowAllPhotoAlbums")->setTitle("Show photo album even if it's empty"));
-		$fields->addFieldToTab("Root.Config", HeaderField::create("Photo Settings"));
-		$fields->addFieldToTab("Root.Config", SliderField::create("PhotosPerPage","Number of Photos Per Page",1,50));
-		$fields->addFieldToTab("Root.Config", SliderField::create("PhotoThumbnailWidth","Photo Thumbnail Width",50,400));
-		$fields->addFieldToTab("Root.Config", SliderField::create("PhotoThumbnailHeight","Photo Thumbnail Height",50,400));
-		$fields->addFieldToTab("Root.Config", SliderField::create("PhotoFullWidth","Photo Fullsize Width",400,1200));
-		$fields->addFieldToTab("Root.Config", SliderField::create("PhotoFullHeight","Photo Fullsize Height",400,1200));
-		return $fields;
-	}
+  public function getCMSFields() {
+    $DefaultAlbumCoverField = UploadField::create('DefaultAlbumCover');
+    $DefaultAlbumCoverField->folderName = "PhotoGallery"; 
+    $DefaultAlbumCoverField->getValidator()->allowedExtensions = array('jpg','jpeg','gif','png');
+    $fields = parent::getCMSFields();
+    $AlbumsGridField = new GridField(
+      "PhotoAlbums",
+      "Album",
+      $this->PhotoAlbums(),
+      GridFieldConfig::create()
+        ->addComponent(new GridFieldToolbarHeader())
+        ->addComponent(new GridFieldAddNewButton('toolbar-header-right'))
+        ->addComponent(new GridFieldSortableHeader())
+        ->addComponent(new GridFieldDataColumns())
+        ->addComponent(new GridFieldPaginator(50))
+        ->addComponent(new GridFieldEditButton())
+        ->addComponent(new GridFieldDeleteAction())
+        ->addComponent(new GridFieldDetailForm())
+        ->addComponent(new GridFieldFilterHeader())
+        ->addComponent($sortable = new GridFieldSortableRows('SortID'))
+    );
+    if($this->AlbumDefaultTop == true) {
+      $sortable->setAppendToTop(true);
+    }
+    $fields->addFieldToTab("Root.Albums", $AlbumsGridField);
+    $fields->addFieldToTab("Root.Config", HeaderField::create("Album Settings"));
+    $fields->addFieldToTab("Root.Config", $DefaultAlbumCoverField);
+    $fields->addFieldToTab("Root.Config", SliderField::create('AlbumsPerPage','Number of Albums Per Page',1,25));
+    $fields->addFieldToTab("Root.Config", SliderField::create("AlbumThumbnailWidth","Album Cover Thumbnail Width",50,400));
+    $fields->addFieldToTab("Root.Config", SliderField::create("AlbumThumbnailHeight","Album Cover Thumbnail Height",50,400));
+    $fields->addFieldToTab("Root.Config", CheckboxField::create("ShowAllPhotoAlbums")->setTitle("Show photo album even if it's empty"));
+    $fields->addFieldToTab("Root.Config", CheckboxField::create("AlbumDefaultTop")->setTitle("Sort new albums to the top by default"));
+    $fields->addFieldToTab("Root.Config", HeaderField::create("Photo Settings"));
+    $fields->addFieldToTab("Root.Config", SliderField::create("PhotosPerPage","Number of Photos Per Page",1,50));
+    $fields->addFieldToTab("Root.Config", SliderField::create("PhotoThumbnailWidth","Photo Thumbnail Width",50,400));
+    $fields->addFieldToTab("Root.Config", SliderField::create("PhotoThumbnailHeight","Photo Thumbnail Height",50,400));
+    $fields->addFieldToTab("Root.Config", SliderField::create("PhotoFullWidth","Photo Fullsize Width",400,1200));
+    $fields->addFieldToTab("Root.Config", SliderField::create("PhotoFullHeight","Photo Fullsize Height",400,1200));
+    $fields->addFieldToTab("Root.Config", CheckboxField::create("PhotoDefaultTop")->setTitle("Sort new photos to the top by default"));
+    return $fields;
+  }
  
 }
  
 class PhotoGallery_Controller extends Page_Controller {
 
    public static function load_requirements() {
-		Requirements::CSS("photogallery/shadowbox/shadowbox.css");
-		Requirements::CSS("photogallery/css/photogallery.css");
-		Requirements::javascript(FRAMEWORK_DIR .'/thirdparty/jquery/jquery.js');
-		Requirements::javascript("photogallery/shadowbox/shadowbox.js");
-		Requirements::javascript("photogallery/js/shadowbox_init.js");
-	}
+    Requirements::CSS("photogallery/shadowbox/shadowbox.css");
+    Requirements::CSS("photogallery/css/photogallery.css");
+    Requirements::javascript(FRAMEWORK_DIR .'/thirdparty/jquery/jquery.js');
+    Requirements::javascript("photogallery/shadowbox/shadowbox.js");
+    Requirements::javascript("photogallery/js/shadowbox_init.js");
+  }
 
-	public function init() {
-		parent::init();
-		self::load_requirements();
-	}
+  public function init() {
+    parent::init();
+    self::load_requirements();
+  }
    
-	private static $allowed_actions = array(
-		'album'
-	); 
-	
-	public function getAlbum() {
-		$Params = $this->getURLParams();
-		if ( is_numeric($Params["ID"]) && $Album = PhotoAlbum::get()->byID((int)$Params["ID"]) ) {  
-			return $Album;
-		}  
-		else {
-			return $this->PhotoAlbums()->first();
-		}
-	}
-	
-	public function album() {      
-		if($PhotoAlbum = $this->getAlbum()) {
-			$Data = array("PhotoAlbum" => $PhotoAlbum);
-			return $this->Customise($Data);
-		}
-		else {
-			return $this->httpError(404, "Sorry that photo album could not be found");
-		}
-	}
+  private static $allowed_actions = array(
+    'album'
+  ); 
+  
+  public function getAlbum() {
+    $Params = $this->getURLParams();
+    if ( is_numeric($Params["ID"]) && $Album = PhotoAlbum::get()->byID((int)$Params["ID"]) ) {  
+      return $Album;
+    }  
+    else {
+      return $this->PhotoAlbums()->first();
+    }
+  }
+  
+  public function album() {      
+    if($PhotoAlbum = $this->getAlbum()) {
+      $Data = array("PhotoAlbum" => $PhotoAlbum);
+      return $this->Customise($Data);
+    }
+    else {
+      return $this->httpError(404, "Sorry that photo album could not be found");
+    }
+  }
 
-	public function AllPhotoAlbums() {
-		return PhotoAlbum::get()->filter("PhotoGalleryID",$this->ID);
-	}
+  public function AllPhotoAlbums() {
+    return PhotoAlbum::get()->filter("PhotoGalleryID",$this->ID);
+  }
 
-	public function PopulatedPhotoAlbums() {
-		$PhotoAlbums = PhotoAlbum::get()->filter("PhotoGalleryID",$this->ID);
-		$PhotoAlbumSet = new ArrayList();
-		if($PhotoAlbums->exists()) {
-			foreach($PhotoAlbums as $PhotoAlbum) {
-				if($PhotoAlbum->getComponents("PhotoItems")->exists() AND $PhotoAlbum->getComponent("PhotoGallery")->exists())
-			   $PhotoAlbumSet -> push($PhotoAlbum); 
-			}
-		}
-		return $PhotoAlbumSet;
-	}
+  public function PopulatedPhotoAlbums() {
+    $PhotoAlbums = PhotoAlbum::get()->filter("PhotoGalleryID",$this->ID);
+    $PhotoAlbumSet = new ArrayList();
+    if($PhotoAlbums->exists()) {
+      foreach($PhotoAlbums as $PhotoAlbum) {
+        if($PhotoAlbum->getComponents("PhotoItems")->exists() AND $PhotoAlbum->getComponent("PhotoGallery")->exists())
+         $PhotoAlbumSet -> push($PhotoAlbum); 
+      }
+    }
+    return $PhotoAlbumSet;
+  }
 
-	public function PhotoAlbums() { 
-		if($this->ShowAllPhotoAlbums){
-			return $this->AllPhotoAlbums();
-		}
-		else {
-			return $this->PopulatedPhotoAlbums();
-		}
-	}
+  public function PhotoAlbums() { 
+    if($this->ShowAllPhotoAlbums){
+      return $this->AllPhotoAlbums();
+    }
+    else {
+      return $this->PopulatedPhotoAlbums();
+    }
+  }
    
-	public function PaginatedAlbums() {
-		$paginatedalbums = new PaginatedList($this->PhotoAlbums(), $this->request);
-		$paginatedalbums->setPageLength($this->AlbumsPerPage);
-		return $paginatedalbums;
-	}
+  public function PaginatedAlbums() {
+    $paginatedalbums = new PaginatedList($this->PhotoAlbums(), $this->request);
+    $paginatedalbums->setPageLength($this->AlbumsPerPage);
+    return $paginatedalbums;
+  }
 
-	public function AlbumCount() {
-		return $this->PhotoAlbums()->count();
-	}
+  public function AlbumCount() {
+    return $this->PhotoAlbums()->count();
+  }
 
-	public function Photos() {
-		return $this->getAlbum()->Photos();
-	}
+  public function Photos() {
+    return $this->getAlbum()->Photos();
+  }
    
-	public function PaginatedPhotos() {
-		$paginatedphotos = new PaginatedList($this->Photos(), $this->request);
-		$paginatedphotos->setPageLength($this->PhotosPerPage);
-		return $paginatedphotos;
-	}
+  public function PaginatedPhotos() {
+    $paginatedphotos = new PaginatedList($this->Photos(), $this->request);
+    $paginatedphotos->setPageLength($this->PhotosPerPage);
+    return $paginatedphotos;
+  }
    
-	public function OtherAlbums() {
-		$OtherAlbums = PhotoAlbum::get()->exclude("ID",$this->getAlbum()->ID)->filter("PhotoGalleryID",$this->ID)->limit("10");
-		$OtherAlbumSet = new ArrayList();
-		if($OtherAlbums->exists()) {
-			foreach($OtherAlbums as $OtherAlbum) {
-				if($OtherAlbum->getComponents("PhotoItems")->exists() AND $OtherAlbum->getComponent("PhotoGallery")->exists() AND $OtherAlbum->getComponent("AlbumCover")->exists())
-				$OtherAlbumSet -> push($OtherAlbum); 
-			}
-		}
-		return $OtherAlbumSet;
-	}
+  public function OtherAlbums() {
+    $OtherAlbums = PhotoAlbum::get()->exclude("ID",$this->getAlbum()->ID)->filter("PhotoGalleryID",$this->ID)->limit("10");
+    $OtherAlbumSet = new ArrayList();
+    if($OtherAlbums->exists()) {
+      foreach($OtherAlbums as $OtherAlbum) {
+        if($OtherAlbum->getComponents("PhotoItems")->exists() AND $OtherAlbum->getComponent("PhotoGallery")->exists() AND $OtherAlbum->getComponent("AlbumCover")->exists())
+        $OtherAlbumSet -> push($OtherAlbum); 
+      }
+    }
+    return $OtherAlbumSet;
+  }
  
 }
