ed1123a13d6640843b5fc857bdc675ced3cbea74
Andrew Houle <andrewhoule@gmail.com>
2018-08-13 11:31:48 -0400

    Dropping reliance on sliderfield

--- code/PhotoGallery.php
+++ code/PhotoGallery.php
@@ -70,9 +70,9 @@ class PhotoGallery extends Page
             ));
         $fields->addFieldsToTab('Root.AlbumSettings', array (
             $defaultAlbumCoverField,
-            SliderField::create('AlbumsPerPage', 'Albums Per Page', 1, 100, $this->AlbumsPerPage),
-            SliderField::create('AlbumThumbnailWidth', 'Album Cover Thumbnail Width', 50, 400, $this->AlbumThumbnailWidth),
-            SliderField::create('AlbumThumbnailHeight', 'Album Cover Thumbnail Height', 50, 400, $this->AlbumThumbnailHeight),
+            NumericField::create('AlbumsPerPage'),
+            NumericField::create('AlbumThumbnailWidth'),
+            NumericField::create('AlbumThumbnailHeight'),
             CheckboxField::create('ShowAllPhotoAlbums', $this->ShowAllPhotoAlbums)
                 ->setTitle('Show photo album even if it\'s empty'),
             CheckboxField::create('AlbumDefaultTop', $this->AlbumDefaultTop)
@@ -80,11 +80,11 @@ class PhotoGallery extends Page
         ));
 
         $fields->addFieldsToTab('Root.PhotoSettings', array (
-            SliderField::create('PhotosPerPage', 'Photos Per Page', 1, 50, $this->PhotosPerPage),
-            SliderField::create('PhotoThumbnailWidth', 'Photo Thumbnail Width', 50, 400, $this->PhotoThumbnailWidth),
-            SliderField::create('PhotoThumbnailHeight', 'Photo Thumbnail Height', 50, 400, $this->PhotoThumbnailHeight),
-            SliderField::create('PhotoFullWidth', 'Photo Fullsize Width', 400, 1900, $this->PhotoFullWidth),
-            SliderField::create('PhotoFullHeight', 'Photo Fullsize Height', 400, 1200, $this->PhotoFullHeight),
+            NumericField::create('PhotosPerPage'),
+            NumericField::create('PhotoThumbnailWidth'),
+            NumericField::create('PhotoThumbnailHeight'),
+            NumericField::create('PhotoFullWidth'),
+            NumericField::create('PhotoFullHeight'),
             CheckboxField::create('PhotoDefaultTop', $this->PhotoDefaultTop)
                 ->setTitle('Sort new photos to the top by default')
         ));
@@ -117,8 +117,9 @@ class PhotoGallery_Controller extends Page_Controller
 
     public function getAlbum()
     {
-        $Params = $this->getURLParams();
-        if (is_numeric($Params['ID']) && $Album = PhotoAlbum::get()->byID((int)$Params['ID'])) {
+        $request = $this->getRequest();
+        $id = $request->param('ID');
+        if (is_numeric($id) && $Album = PhotoAlbum::get()->byID((int)$id)) {
             return $Album;
         } else {
             return $this->PhotoAlbums()->first();
